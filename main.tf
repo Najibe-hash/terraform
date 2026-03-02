133efda91f01b75df34a5ff564bd840809f0f9e9
Najibe-hash <najibevisticot@proton.me>
2026-03-02 10:25:02 +0000

    Ajout bucket S3

--- main.tf
+++ main.tf
@@ -43,3 +43,83 @@ resource "aws_instance" "ec2_visticot" {
 output "ec2_public_ip" {
   value = aws_instance.ec2_visticot.public_ip
 }
+
+resource "aws_s3_bucket" "ucad_bucket" {
+  bucket = "S3-Visticot"
+}
+
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.ucad_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
+resource "aws_s3_bucket_website_configuration" "static_site" {
+  bucket = aws_s3_bucket.ucad_bucket.id
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+
+resource "aws_s3_bucket_lifecycle_configuration" "ucad_lifecycle" {
+  bucket = aws_s3_bucket.ucad_bucket.id
+
+  rule {
+    id     = "archive_and_delete_rule"
+    status = "Enabled"
+
+    transition {
+      days          = 90
+      storage_class = "STANDARD_IA"
+    }
+
+    expiration {
+      days = 365
+    }
+  }
+}
+
+
+resource "aws_s3_object" "file_txt" {
+  bucket  = aws_s3_bucket.ucad_bucket.id
+  key     = "devoir.txt"
+  content = "Ceci est le fichier texte pour le devoir Master 2 informatique UCAD et appartient à Visticot Najibe."
+}
+
+resource "aws_s3_object" "index_html" {
+  bucket       = aws_s3_bucket.ucad_bucket.id
+  key          = "index.html"
+  source       = "index.html"
+  content_type = "text/html"
+}
+
+
+resource "aws_s3_bucket_public_access_block" "allow_public" {
+  bucket = aws_s3_bucket.ucad_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "public_read" {
+  depends_on = [aws_s3_bucket_public_access_block.allow_public]
+  bucket     = aws_s3_bucket.ucad_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.ucad_bucket.arn}/*"
+      },
+    ]
+  })
+}
